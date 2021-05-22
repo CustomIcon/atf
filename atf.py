0f5114557d24ec6fc948f196ba6cc9cbc2c926f8
ポキ <aman_a@aol.com>
2021-05-22 19:39:16 +0500

    Initial Commit

---
+++ atf.py
@@ -0,0 +1,35 @@
+class Formatter:
+    def __init__(
+        self,
+        title: str=None,
+        desctription: str=None,
+        bulletpoints: list=None,
+    ):
+        """
+        [ATF Formatter]
+
+        Args:
+            title (str, optional): [Title]. Defaults to None.
+            desctription (str, optional): [Description]. Defaults to None.
+            bulletpoints (list, optional): [Bullet Points]. Defaults to None.
+        """
+        self.title = title
+        self.description = desctription
+        self.bulletpoints = bulletpoints
+        self.form = '{}{}{}'
+    
+    def save(self, markdown:bool=True):
+        if self.bulletpoints:
+            self.bulletpoints.insert(0,'')
+        if markdown:
+            return (self.form.format(
+                "**--" + self.title + "--**" if self.title else "",
+                "\n" + "__" + self.description + "__"  if self.description else "",
+                "\n" + "\n• ".join(self.bulletpoints) if self.bulletpoints else ""
+            ))
+        else:
+            return self.form.format(
+                "<b><u>" + self.title + "</u></b>" if self.title else "",
+                "\n" + "<i>" + self.description + "</i>"  if self.description else "",
+                "\n" + "\n• ".join(self.bulletpoints) if self.bulletpoints else ""
+                )
